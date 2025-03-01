d7ee00f87e3383a31eb3abd1a8337740970f057c
Ian Magonya <ianbenard2002@gmail.com>
2025-03-01 16:45:13 +0300

    added all the reqiured pages

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,39 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::get('/', function(){
-    return view('layouts.home');
+Route::get('/', function () {
+    return view('home');
 });
+
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
+
+
+Route::get('/blog', function () {
+    return view('blog');
+})->name('blog');
+
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact');
+
+Route::get('/event', function () {
+    return view('event');
+})->name('event');
+
+Route::get('/event-detail', function () {
+    return view('event-detail');
+})->name('event-detail');
+
+Route::get('/teacher', function () {
+    return view('teacher');
+})->name('teacher');
+
+Route::get('/blog-list', function () {
+    return view('listblog');
+})->name('listblog');
+
+Route::get('/blog-detail', function () {
+    return view('index');
+})->name('blog-detail');
